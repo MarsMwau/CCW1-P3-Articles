d0a61d0756cdb0149a7ead3e8f26b1efbacae413
MarsMwau <marthamumbua16@gmail.com>
2023-05-28 00:55:33 +0300

    Working on the Author class

--- lib/Article.rb
+++ lib/Article.rb
@@ -1,19 +1,16 @@
 class Article
     attr_accessor :author, :magazine, :title
+    @@all_articles = []
 
     def initialize(author:, magazine: title:)
         @author = author
         @magazine = magazine
         @title = title
-        @@all
-    end
-
-    def title
-        @title
+        @@all_articles << self
     end
 
     def self.all
-        @@all = []
+        @@all_articles
     end
 
 end
--- lib/Magazine.rb
+++ lib/Magazine.rb
@@ -1,35 +1,34 @@
 class Magazine
-    attr_reader :name, :category
-    @@all_magazines = []
-  
-    def initialize(name, category)
-      @name = name
-      @category = category
-      @@all_magazines << self
-    end
-  
-    def self.all
-      @@all_magazines
-    end
-  
-    def self.find_by_name(name)
-      @@all_magazines.find { |magazine| magazine.name == name }
-    end
-  
-    def contributors
-      Article.all.select { |article| article.magazine == self }.map { |article| article.author }
-    end
-  
-    def article_titles
-      Article.all.select { |article| article.magazine == self }.map { |article| article.title }
-    end
-  
-    def name=(new_name)
-      @name = new_name
-    end
-  
-    def category=(new_category)
-      @category = new_category
-    end
+  attr_reader :name, :category
+  @@all_magazines = []
+
+  def initialize(name, category)
+    @name = name
+    @category = category
+    @@all_magazines << self
   end
-  
+
+  def self.all
+    @@all_magazines
+  end
+
+  def self.find_by_name(name)
+    @@all_magazines.find { |magazine| magazine.name == name }
+  end
+
+  def contributors
+    Article.all.select { |article| article.magazine == self }.map { |article| article.author }
+  end
+
+  def article_titles
+    Article.all.select { |article| article.magazine == self }.map { |article| article.title }
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def category=(new_category)
+    @category = new_category
+  end
+end
