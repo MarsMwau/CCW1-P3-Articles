ac7d6ba9fb28186e927cd72ea01aa4b06c13eafe
MarsMwau <marthamumbua16@gmail.com>
2023-05-28 00:47:47 +0300

    Working on the Magazine class

--- lib/Magazine.rb
+++ lib/Magazine.rb
@@ -1,22 +1,35 @@
 class Magazine
-    attr_accessor :name, :category
-
+    attr_reader :name, :category
+    @@all_magazines = []
+  
     def initialize(name, category)
-        @name = name
-        @category = category
-        @@all
+      @name = name
+      @category = category
+      @@all_magazines << self
     end
-
-    def name
-        @name
-    end
-
-    def category
-        @category
-    end
-
+  
     def self.all
-        @@all = self.
+      @@all_magazines
     end
-
-end
+  
+    def self.find_by_name(name)
+      @@all_magazines.find { |magazine| magazine.name == name }
+    end
+  
+    def contributors
+      Article.all.select { |article| article.magazine == self }.map { |article| article.author }
+    end
+  
+    def article_titles
+      Article.all.select { |article| article.magazine == self }.map { |article| article.title }
+    end
+  
+    def name=(new_name)
+      @name = new_name
+    end
+  
+    def category=(new_category)
+      @category = new_category
+    end
+  end
+  
